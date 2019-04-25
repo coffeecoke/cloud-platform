de98a5e4d68c1b0d601eadf7527be4334ba0e689
coffeecoke <18601376034@163.com>
2019-04-25 15:30:29 +0800

    用户管理左侧菜单

--- src/views/personal-management/components/aside-menu.vue
+++ src/views/personal-management/components/aside-menu.vue
@@ -4,22 +4,22 @@
     <el-submenu index="partner1">
       <template slot="title"><i class="el-icon-message"></i>融鑫合伙人</template>
       <el-menu-item-group>
-        <el-menu-item index="partner-1">用户列表</el-menu-item>
-        <el-menu-item index="partner-2">用户评级</el-menu-item>
+        <el-menu-item index="partner-1">人员列表</el-menu-item>
+        <el-menu-item index="partner-2">能力结构</el-menu-item>
       </el-menu-item-group>
     </el-submenu>
     <el-submenu index="partner2">
       <template slot="title"><i class="el-icon-message"></i>企业合伙人</template>
       <el-menu-item-group>
         <el-menu-item index="partner-3">企业列表</el-menu-item>
-        <el-menu-item index="partner-4">企业评级</el-menu-item>
+        <el-menu-item index="partner-4">企业能力评价</el-menu-item>
       </el-menu-item-group>
     </el-submenu>
     <el-submenu index="partner3">
       <template slot="title"><i class="el-icon-message"></i>个人合伙人</template>
       <el-menu-item-group>
-        <el-menu-item index="partner-5">用户列表</el-menu-item>
-        <el-menu-item index="partner-6">用户评级</el-menu-item>
+        <el-menu-item index="partner-5">人员列表</el-menu-item>
+        <el-menu-item index="partner-6">能力结构</el-menu-item>
       </el-menu-item-group>
     </el-submenu>
   </el-menu>
