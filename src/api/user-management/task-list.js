30392c3b69988db1516b19f3816109f5333c0aca
zuokai <zuokai32@163.com>
2019-04-26 10:33:39 +0800

    分支提交zk

--- src/api/user-management/task-list.js
+++ src/api/user-management/task-list.js
@@ -10,11 +10,51 @@ const TaskList = {
       data: params
     })
   },
-  // 任务发布查询
+  // 任务列表查询
   getTaskList (params) {
     return axios({
       method: 'post',
       url: '/taskSet/getTaskList',
+      data: params
+    })
+  },
+  // 部门查询
+  getTaskPersonList (params) {
+    return axios({
+      method: 'post',
+      url: '/taskSet/getTaskPersonList',
+      data: params
+    })
+  },
+  // 任务列表分配
+  saveSubordinatePerson (params) {
+    return axios({
+      method: 'post',
+      url: '/taskSet/saveSubordinatePerson',
+      data: params
+    })
+  },
+  // 任务列表设置属组
+  saveMultTaskGroup (params) {
+    return axios({
+      method: 'post',
+      url: '/taskSet/saveMultTaskGroup',
+      data: params
+    })
+  },
+  // 审核操作
+  auditTask (params) {
+    return axios({
+      method: 'post',
+      url: '/taskSet/auditTask',
+      data: params
+    })
+  },
+  // 验收操作
+  acceptTask (params) {
+    return axios({
+      method: 'post',
+      url: '/taskSet/acceptTask',
       data: params
     })
   }
