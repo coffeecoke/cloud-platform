c7572d0aa9ecd2380a0374e7889d92fbbf9c119c
coffeecoke <18601376034@163.com>
2019-05-28 17:41:56 +0800

    表逻辑顶部操作按钮

--- src/views/table-logic/components/select-btns.vue
+++ src/views/table-logic/components/select-btns.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="" id="">
     <label for="" v-for="(item,index) in selectKeys" :key="index">
-      <input type="text">
+      <input type="text" class="select-input">
       <span>item</span>
     </label>
   </div>
 </template>
 <script>
 export default {
-  props: { selectKeys },
+  // props: { selectKeys },
   data () {
     return {
+      selectKeys: ['字段名称', '类型', '主键', '中文名称', '字段说明']
     }
   },
   activated () {
@@ -19,4 +20,7 @@ export default {
 </script>
 <style scoped lang="scss">
 /* css */
+.select-input {
+  display:none;
+}
 </style>
--- src/views/table-logic/tableLogic.vue
+++ src/views/table-logic/tableLogic.vue
@@ -1,18 +1,123 @@
 <template>
   <div class="" id="">
-    表逻辑
+    <div class="table-top">
+      <div class="field-screening">
+        <h3>字段筛选：</h3>
+        <el-checkbox-group v-model="checkboxGroup" size="mini" @change='secletChange'>
+          <el-checkbox v-for="(item,index) in tableListKeys" :key="index" :label="item" border></el-checkbox>
+        </el-checkbox-group>
+      </div>
+      <div class="operation-btns">
+        <el-button>新增字段</el-button>
+        <el-button type="primary">保存</el-button>
+        <el-button>导入</el-button>
+        <el-button>导出</el-button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
+      // 默认选中的字段
+      checkboxGroup: ['新增字段', '类型'],
+      // 表格中的key数组
+      tableListKeys: ['新增字段', '类型', '(主键,可空)', '中文名称', '字段', '值域', '取数逻辑', '访谈纪要', '操作', '问题数', '状态', '系统.表.字段',
+        '业务逻辑', '处理逻辑', '备注'
+      ],
+      tableColumn: [{
+        name: '新增字段',
+        show: true
+      },
+      {
+        name: '类型',
+        show: true
+      },
+      {
+        name: '(主键,可空)',
+        show: true
+      },
+      {
+        name: '中文名称',
+        show: true
+      },
+      {
+        name: '字段',
+        show: true
+      },
+      {
+        name: '值域',
+        show: true
+      },
+      {
+        name: '取数逻辑',
+        show: true
+      },
+      {
+        name: '操作',
+        show: true
+      },
+      {
+        name: '问题数',
+        show: true
+      },
+      {
+        name: '状态',
+        show: true
+      },
+      {
+        name: '系统.表.字段',
+        show: true
+      },
+      {
+        name: '业务逻辑',
+        show: true
+      },
+      {
+        name: '处理逻辑',
+        show: true
+      },
+      {
+        name: '备注',
+        show: true
+      }
+
+      ],
+
+      tableList: [
+        {
+          name: ''
+        }
+      ]
     }
   },
-  activated () {
-  }
+  methods: {
+    // 字段变更后的回调
+    secletChange (selectedArr) {
+      this.checkboxGroup = selectedArr
+    }
+  },
+  activated () {}
 }
+
 </script>
 <style scoped lang="scss">
-/* css */
+  /* css */
+  .table-top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+
+    .field-screening {
+      display: flex;
+      align-items: center;
+    }
+
+    .el-checkbox {
+      margin-right: 0px;
+    }
+  }
+
 </style>
