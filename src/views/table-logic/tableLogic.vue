74ee3cd7ed9aaf5180dff493fe1896a6b010b6d8
coffeecoke <zhaoqian_0828@163.com>
2019-05-29 18:17:58 +0800

    ym

--- src/views/table-logic/tableLogic.vue
+++ src/views/table-logic/tableLogic.vue
@@ -9,7 +9,7 @@
       </div>
       <div class="operation-btns">
         <el-button>新增字段</el-button>
-        <el-button type="primary">保存</el-button>
+        <el-button type="primary" @click="saveClick()">保存</el-button>
         <el-button>导入</el-button>
         <el-button>导出</el-button>
       </div>
@@ -92,13 +92,14 @@
       </el-table-column>
       <el-table-column prop="state" :label="tableColumn[10].name" v-if="tableColumn[10].show" width="120">
         <template slot-scope="scope">
-           <template v-if="scope.row.edit">
+          <template v-if="scope.row.edit">
             <el-select v-model="scope.row.state">
-              <el-option v-for="item in state" :key="item.dictCode" :label="item.dictName"
-                :value="item.dictCode" :disabled="item.disabled">
+              <el-option v-for="item in state" :key="item.dictCode" :label="item.dictName" :value="item.dictCode"
+                :disabled="item.disabled">
               </el-option>
             </el-select>
           </template>
+          <span v-else>{{scope.row.state}}</span>
         </template>
       </el-table-column>
       <el-table-column prop="system" :label="tableColumn[11].name" v-if="tableColumn[11].show" width="120">
@@ -111,15 +112,31 @@
           <span>{{ scope.row.spring }}</span>
         </template>
       </el-table-column>
-      <el-table-column prop="processLogic" :label="tableColumn[13].name" v-if="tableColumn[13].show" width="120">
+      <el-table-column prop="processLogic" :label="tableColumn[13].name" v-if="tableColumn[13].show" width="400">
         <template slot-scope="scope">
-           <template v-if="scope.row.edit">
-            <el-select v-model="scope.row.processLogic">
+          <template v-if="scope.row.editList">
+            <el-autocomplete
+              class="inline-input"
+              v-model="state1"
+              :fetch-suggestions="querySearch"
+              placeholder="请输入内容"
+              @select="handleSelect"
+            ></el-autocomplete>
+            <!-- <el-select v-model="scope.row.processLogic" multiple filterable remote reserve-keyword placeholder="请输入关键词"
+              :remote-method="remoteMethod" :loading="loading">
+              <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+              </el-option>
+            </el-select> -->
+            <!-- <el-input type="text" v-model="scope.row.processLogic" ><i class="el-icon-edit"></i></el-input> -->
+            <!-- <el-select v-model="scope.row.processLogic">
               <el-option v-for="item in processLogic" :key="item.dictCode" :label="item.dictName"
                 :value="item.dictCode" :disabled="item.disabled">
               </el-option>
-            </el-select>
+            </el-select> -->
           </template>
+          <div v-else>
+            {{scope.row.processLogic}}
+          </div>
         </template>
       </el-table-column>
       <el-table-column prop="remarks" :label="tableColumn[14].name" v-if="tableColumn[14].show" width="120">
@@ -137,6 +154,8 @@ export default {
     return {
       // 默认选中的字段
       checkboxGroup: [],
+      restaurants: [],
+      state1: '',
       // table 列
       tableColumn: [{
         name: '新增字段',
@@ -207,98 +226,105 @@ export default {
         name: 'DATA_DATE',
         type: 'CHA',
         text: '(a)',
-        processLogic: '1',
+        processLogic: 'aaa',
         state: '3',
         Cname: '数据日期',
         explain: '字段说明字段说明字段说明字段说明字段说明字段说',
         range: '数据日期',
         summary: '访谈纪要访谈纪要访谈纪要访谈纪要',
         question: '1',
-        edit: true
+        edit: true,
+        editList: true
       }, {
         id: 2,
         number: '2',
         name: 'CUST_NAM_EN',
         type: 'CHAR(6)',
         text: '(a)',
-        processLogic: '1',
+        processLogic: '',
         state: '1',
         Cname: '客户英文名称',
         explain: '字段说明字段说明字段说明字段说明字段说明字段说',
         range: '客户英文名称',
         summary: '访谈纪要访谈纪要访谈纪要访谈纪要',
         question: '1',
-        edit: true
+        edit: true,
+        editList: true
       }, {
         id: 3,
         number: '3',
         name: 'LALALLA',
         type: 'CHAR(2)',
         text: '(a)',
-        processLogic: '1',
+        processLogic: '',
         state: '1',
         Cname: '境内境外标志',
         explain: '字段说明字段说明字段说明字段说明字段说明字段说',
         range: '境内境外标志',
         summary: '访谈纪要访谈纪要访谈纪要访谈纪要',
         question: '3',
-        edit: true
+        edit: true,
+        editList: true
       }, {
         id: 4,
         number: '4',
         name: 'HEHASI',
         type: 'INT',
         text: '(a)',
-        processLogic: '1',
+        processLogic: '',
         state: '1',
         Cname: '客户大类',
         explain: '字段说明字段说明字段说明字段说明字段说明字段说',
         range: '客户大类',
         summary: '访谈纪要访谈纪要访谈纪要访谈纪要',
         question: '1',
-        edit: true
+        edit: true,
+        editList: true
       }, {
         id: 5,
         number: '5',
         name: 'FDEII',
         type: 'VARCHAR2(200)',
         text: '(a)',
-        processLogic: '1',
+        processLogic: '',
         state: '1',
         Cname: '行政代码',
         explain: '字段说明字段说明字段说明字段说明字段说明字段说',
         range: '行政代码',
         summary: '访谈纪要访谈纪要访谈纪要访谈纪要',
         question: '7',
-        edit: true
+        edit: true,
+        editList: true
       }, {
         id: 6,
         number: '6',
         name: 'DATA',
         type: 'CHAR(1)',
         text: '(a)',
-        processLogic: '1',
+        processLogic: '',
         state: '1',
         Cname: '数据日期',
         explain: '字段说明字段说明字段说明字段说明字段说明字段说',
         range: '数据日期',
         summary: '访谈纪要访谈纪要访谈纪要访谈纪要',
         question: '4',
-        edit: true
+        edit: true,
+        editList: true
       }, {
         id: 7,
         number: '7',
         name: 'DATA_DATE',
         type: 'CHAR(2)',
         text: '(a)',
-        processLogic: '3',
+        processLogic: '',
         state: '1',
         Cname: '境内境外标志',
         explain: '字段说明字段说明字段说明字段说明字段说明字段说',
         range: '境内境外标志',
         summary: '访谈纪要访谈纪要访谈纪要访谈纪要',
         question: '1',
-        edit: true
+        edit: true,
+        editList: true
       }],
       processLogic: [{
         dictCode: '1',
@@ -315,7 +341,8 @@ export default {
       {
         dictCode: '4',
         dictName: '已确认'
-      }],
+      }
+      ],
       state: [{
         dictCode: '1',
         dictName: '未提交'
@@ -331,7 +358,8 @@ export default {
       {
         dictCode: '4',
         dictName: '已确认'
-      }]
+      }
+      ]
     }
   },
   // created () {
@@ -348,6 +376,18 @@ export default {
   //   })
   // },
   methods: {
+    formatProcessLogic (value) {
+      let currObj = this.processLogic.filter(obj => {
+        return obj.dictCode === value
+      })
+      return currObj.length > 0 ? currObj[0].dictName : ''
+    },
+    formatState (value) {
+      let currObj = this.state.filter(obj => {
+        return obj.dictCode === value
+      })
+      return currObj.length > 0 ? currObj[0].dictName : ''
+    },
     toggleColumn () {
       // 隐藏列之前先把每列显示出来
       this.tableColumn.forEach((item, index) => {
@@ -365,6 +405,51 @@ export default {
     secletChange (selectedArr) {
       this.checkboxGroup = selectedArr
       this.toggleColumn()
+    },
+    // 模糊搜索
+    remoteMethod (query) {
+      if (query !== '') {
+        this.loading = true
+        setTimeout(() => {
+          this.loading = false
+          this.options = this.list.filter(item => {
+            return item.label.toLowerCase()
+              .indexOf(query.toLowerCase()) > -1
+          })
+        }, 200)
+      } else {
+        this.options = []
+      }
+    },
+    saveClick () {
+      this.tableData.forEach((item, index) => {
+        console.log(item)
+        item.editList = false
+        item.edit = false
+      })
+    },
+    querySearch (queryString, cb) {
+      var restaurants = this.restaurants
+      var results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants
+      // 调用 callback 返回建议列表的数据
+      cb(results)
+    },
+    createFilter (queryString) {
+      return (restaurant) => {
+        return (restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0)
+      }
+    },
+    loadAll () {
+      return [
+        {value: 'aaaaa'},
+        {value: 'abccbcbcb'},
+        {value: 'sssss'},
+        {value: 'dddccc'},
+        {value: 'qqqqq'}
+      ]
+    },
+    handleSelect (item) {
+      console.log(item)
     }
   },
   computed: {
@@ -379,6 +464,13 @@ export default {
     }
   },
   mounted () {
+    this.restaurants = this.loadAll()
+    this.list = this.states.map(item => {
+      return {
+        value: item,
+        label: item
+      }
+    })
     // 默认显示全部列
     this.tableColumn.forEach((item, index) => {
       this.checkboxGroup.push(item.name)
@@ -428,14 +520,17 @@ export default {
       }
     }
   }
-.text-color{
-  color:#afcffb;
-}
-  .el-table /deep/ .el-table__header {
-    min-width:100%
+
+  .text-color {
+    color: #afcffb;
   }
+
+  .el-table /deep/ .el-table__header {
+    min-width: 100%
+  }
+
   .el-table /deep/ .el-table__body {
-    min-width:100%
+    min-width: 100%
   }
 
 </style>
