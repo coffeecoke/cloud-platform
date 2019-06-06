d99310cd5f7fe9047247331f350d567adbb79558
coffeecoke <zhaoqian_0828@163.com>
2019-06-06 18:15:05 +0800

    ym

--- src/views/table-logic/components/add-field.vue
+++ src/views/table-logic/components/add-field.vue
@@ -1,97 +1,85 @@
 <template>
-    <div class="name-box" v-show="isShowAllField">
-        <div class="mask">
-            <div class="mask-right">
-                <div class="mask-right-icon" @click="AddBoxHide">
-                    <i class="fa el-icon-arrow-right"></i>
-                </div>
-                <div class="mask-right-content">
-                    <div class="title-box">
-                        <h4><i class="fa fa-file-text"></i><span>字段信息</span></h4>
-                        <el-button class="button" @click="submitForm('ruleForm')"><i class="fa fa-save"></i><span>保存</span></el-button>
-                    </div>
-                    <div class="content-box">
-                        <div class="form-box">
-                            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-                                <el-form-item label="字段名称:" prop="name">
-                                    <el-input v-model="ruleForm.name"></el-input>
-                                </el-form-item>
-                                <el-form-item label="类型:" prop="type">
-                                    <el-input v-model="ruleForm.type"></el-input>
-                                </el-form-item>
-                                <el-form-item label="长度:" prop="length">
-                                    <el-input v-model="ruleForm.length"></el-input>
-                                </el-form-item>
-                                <el-form-item label="主键:" prop="majorKey">
-                                    <el-select v-model="ruleForm.majorKey" placeholder="请选择活动区域">
-                                        <el-option label="区域一" value="shanghai"></el-option>
-                                        <el-option label="区域二" value="beijing"></el-option>
-                                    </el-select>
-                                </el-form-item>
-                                <el-form-item label="是否可空:" prop="null">
-                                    <el-select v-model="ruleForm.null" placeholder="请选择活动区域">
-                                        <el-option label="区域一" value="shanghai"></el-option>
-                                        <el-option label="区域二" value="beijing"></el-option>
-                                    </el-select>
-                                </el-form-item>
-                                <el-form-item label="中文名称:" prop="Cname">
-                                    <el-input v-model="ruleForm.Cname"></el-input>
-                                </el-form-item>
-                                <el-form-item label="字段说明:" prop="explain">
-                                    <el-input type="textarea" v-model="ruleForm.explain"></el-input>
-                                </el-form-item>
-                                <el-form-item label="是否有码值:" prop="code">
-                                    <el-select v-model="ruleForm.code" placeholder="请选择活动区域">
-                                        <el-option label="区域一" value="shanghai"></el-option>
-                                        <el-option label="区域二" value="beijing"></el-option>
-                                    </el-select>
-                                </el-form-item>
-                                <el-form-item label="使用模块:" prop="templ">
-                                    <el-input type="textarea" v-model="ruleForm.templ"></el-input>
-                                </el-form-item>
-                                <el-form-item label="使用位置:" prop="position">
-                                    <el-input type="textarea" v-model="ruleForm.position"></el-input>
-                                </el-form-item>
-                                <el-form-item label="访谈纪要:" prop="summary">
-                                    <el-input v-model="ruleForm.summary"></el-input>
-                                </el-form-item>
-                                <el-form-item label="业务逻辑:" prop="logic">
-                                    <el-input v-model="ruleForm.logic"></el-input>
-                                </el-form-item>
-                                <el-form-item label="备注:" prop="remarks">
-                                    <el-input v-model="ruleForm.remarks"></el-input>
-                                </el-form-item>
-
-                            </el-form>
-                        </div>
-                        <div class="table-box">
-                            <el-table
-                            :data="tableData"
-                            style="width: 100%">
-                            <el-table-column
-                                prop="date"
-                                label="系统"
-                                sortable
-                                width="180">
-                            </el-table-column>
-                            <el-table-column
-                                prop="name"
-                                label="表"
-                                sortable
-                                width="180">
-                            </el-table-column>
-                            <el-table-column
-                                prop="address"
-                                sortable
-                                label="字段">
-                            </el-table-column>
-                            </el-table>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <div class="name-box" v-show="isShowAllField">
+    <div class="mask">
+      <div class="mask-right">
+        <div class="mask-right-icon" @click="AddBoxHide">
+          <i class="fa el-icon-arrow-right"></i>
         </div>
+        <div class="mask-right-content">
+          <div class="title-box">
+            <h4><i class="fa fa-file-text"></i><span>字段信息</span></h4>
+            <el-button class="button" @click="submitForm('ruleForm')"><i class="fa fa-save"></i><span>保存</span>
+            </el-button>
+          </div>
+          <div class="content-box">
+            <div class="form-box">
+              <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+                <el-form-item label="字段名称:" prop="name">
+                  <el-input v-model="ruleForm.name"></el-input>
+                </el-form-item>
+                <el-form-item label="类型:" prop="type">
+                  <el-input v-model="ruleForm.type"></el-input>
+                </el-form-item>
+                <el-form-item label="长度:" prop="length">
+                  <el-input v-model="ruleForm.length"></el-input>
+                </el-form-item>
+                <el-form-item label="主键:" prop="majorKey">
+                  <el-select v-model="ruleForm.majorKey" placeholder="请选择活动区域">
+                    <el-option label="区域一" value="shanghai"></el-option>
+                    <el-option label="区域二" value="beijing"></el-option>
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="是否可空:" prop="null">
+                  <el-select v-model="ruleForm.null" placeholder="请选择活动区域">
+                    <el-option label="区域一" value="shanghai"></el-option>
+                    <el-option label="区域二" value="beijing"></el-option>
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="中文名称:" prop="Cname">
+                  <el-input v-model="ruleForm.Cname"></el-input>
+                </el-form-item>
+                <el-form-item label="字段说明:" prop="explain">
+                  <el-input type="textarea" v-model="ruleForm.explain"></el-input>
+                </el-form-item>
+                <el-form-item label="是否有码值:" prop="code">
+                  <el-select v-model="ruleForm.code" placeholder="请选择活动区域">
+                    <el-option label="区域一" value="shanghai"></el-option>
+                    <el-option label="区域二" value="beijing"></el-option>
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="使用模块:" prop="templ">
+                  <el-input type="textarea" v-model="ruleForm.templ"></el-input>
+                </el-form-item>
+                <el-form-item label="使用位置:" prop="position">
+                  <el-input type="textarea" v-model="ruleForm.position"></el-input>
+                </el-form-item>
+                <el-form-item label="访谈纪要:" prop="summary">
+                  <el-input v-model="ruleForm.summary"></el-input>
+                </el-form-item>
+                <el-form-item label="业务逻辑:" prop="logic">
+                  <el-input v-model="ruleForm.logic"></el-input>
+                </el-form-item>
+                <el-form-item label="备注:" prop="remarks">
+                  <el-input v-model="ruleForm.remarks"></el-input>
+                </el-form-item>
+
+              </el-form>
+            </div>
+            <div class="table-box">
+              <el-table :data="tableData" style="width: 100%">
+                <el-table-column prop="date" label="系统" sortable width="180">
+                </el-table-column>
+                <el-table-column prop="name" label="表" sortable width="180">
+                </el-table-column>
+                <el-table-column prop="address" sortable label="字段">
+                </el-table-column>
+              </el-table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
@@ -114,18 +102,26 @@ export default {
         remarks: ''
       },
       rules: {
-        type: [
-          { required: true, message: '请选择活动区域', trigger: 'blur' }
-        ],
-        majorKey: [
-          { required: true, message: '请选择活动区域', trigger: 'change' }
-        ],
-        null: [
-          { required: true, message: '请选择活动区域', trigger: 'change' }
-        ],
-        Cname: [
-          { required: true, message: '请选择活动区域', trigger: 'blur' }
-        ]
+        type: [{
+          required: true,
+          message: '请选择活动区域',
+          trigger: 'blur'
+        }],
+        majorKey: [{
+          required: true,
+          message: '请选择活动区域',
+          trigger: 'change'
+        }],
+        null: [{
+          required: true,
+          message: '请选择活动区域',
+          trigger: 'change'
+        }],
+        Cname: [{
+          required: true,
+          message: '请选择活动区域',
+          trigger: 'blur'
+        }]
       },
       tableData: [{
         date: '2016-05-02',
@@ -161,73 +157,93 @@ export default {
     }
   }
 }
+
 </script>
 <style scoped lang="scss">
-.mask{
-    width:100%;
-    height:100%;
-    background-color:rgba(0,0,0,0.2);
+  .mask {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.2);
     position: fixed;
-    top:0;
-    left:0;
-    bottom:0;
-    right:0;
-    z-index:1000;
-    .mask-right{
-        width:30%;
-        height:100%;
-        background:#fff;
-        float: right;
-        .mask-right-icon{
-            float: left;
-            width:30px;
-            height:100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            i{
-                color:#3080ee;
-                font-weight:bolder;
-            }
-            &:hover{
-                background: #ddd;
-            }
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 1000;
+
+    .mask-right {
+      width: 30%;
+      height: 100%;
+      background: #fff;
+      float: right;
+
+      .mask-right-icon {
+        float: left;
+        width: 30px;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        i {
+          color: #3080ee;
+          font-weight: bolder;
         }
+
+        &:hover {
+          background: #ddd;
+        }
+      }
     }
-    .mask-right-content{
-        padding:0 10px;
-        margin-left:30px;
-        .title-box{
-            overflow: hidden;
-            border-bottom:solid 1px #ccc;
-            h4{
-                float:left;
-                span{
-                    padding-left:10px;
-                }
-            }
-            .button{
-                float: right;
-                margin-top:15px;
-                span{
-                    padding-left:2px;
-                }
-            }
+
+    .mask-right-content {
+      padding: 0 10px;
+      margin-left: 30px;
+
+      .title-box {
+        overflow: hidden;
+        border-bottom: solid 1px #ccc;
+
+        h4 {
+          float: left;
+
+          span {
+            padding-left: 10px;
+          }
         }
-        .content-box{
-            .text-box{
-                font-size:12px;
-                padding:5px 0;
-            }
+
+        .button {
+          float: right;
+          margin-top: 15px;
+
+          span {
+            padding-left: 2px;
+          }
         }
+      }
+
+      .content-box {
+        .text-box {
+          font-size: 12px;
+          padding: 5px 0;
+        }
+      }
     }
-}
-.form-box{
-    padding:8px 20px;
-    .el-form{
-        .el-form-item{
-            height:22px;
-        }
+  }
+
+  .form-box {
+    padding: 8px 20px;
+
+    .el-form {
+      .el-form-item {
+        height: 22px;
+      }
+
     }
-}
+  }
+
+  .el-form /deep/ .el-textarea__inner {
+    height: 30px;
+  }
+
 </style>
