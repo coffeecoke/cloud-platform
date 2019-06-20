a870bbcd0d39a063f92e9bcd4e6f3592ff6b9af0
coffeecoke <18601376034@163.com>
2019-06-20 17:54:55 +0800

    查看详情

--- src/views/ai-laboratory/components/elastic-frame.vue
+++ src/views/ai-laboratory/components/elastic-frame.vue
@@ -1,128 +1,136 @@
 <template>
-    <div class="elastic-box">
-        <div class="mask">
-            <transition enter-active-class="bounceInRight" leave-active-class="bounceInLeft">
-                <div class="mask-content">
-                    <div class="padding-20">
-                        <div class="box-header">
-                            <p class="details">查看详情</p>
-                            <i class="el-icon-close"></i>
-                        </div>
-                        <div class="box-contenter">
-                            <div class="box-content-left">
-                                nfvnfjv
-                            </div>
-                            <div class="box-content-right">
-                                dfvnjfvbh
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </transition>
-        </div>
-    </div>
+  <el-dialog title="查看详情" :visible.sync="detailDialog" width="882px" class="dialog-container" @close="closeHandel">
+    <el-container>
+      <el-aside width="186px">
+        <el-tree :data="detailTreeData" :props="defaultProps"></el-tree>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-dialog>
 </template>
 <script>
 export default {
+  props: ['detailDialog'],
   data () {
     return {
-      data: [{
-        label: '一级 1',
+      detailTreeData: [{
+        label: '分类1',
         children: [{
-          label: '二级 1-1',
-          children: [{
-            label: '三级 1-1-1'
-          }]
+          label: '二级 1-1'
         }]
       }, {
-        label: '一级 2',
+        label: '分类2',
         children: [{
-          label: '二级 2-1',
-          children: [{
-            label: '三级 2-1-1'
-          }]
+          label: '二级 2-1'
         }, {
-          label: '二级 2-2',
-          children: [{
-            label: '三级 2-2-1'
-          }]
+          label: '二级 2-2'
         }]
       }, {
-        label: '一级 3',
+        label: '分类3',
         children: [{
-          label: '二级 3-1',
-          children: [{
-            label: '三级 3-1-1'
-          }]
+          label: '二级 3-1'
         }, {
-          label: '二级 3-2',
-          children: [{
-            label: '三级 3-2-1'
-          }]
+          label: '二级 3-2'
         }]
-      }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
     }
   },
   methods: {
-    handleNodeClick (data) {
-      console.log(data)
+    closeHandel () {
+      this.$emit('detailBoxHide')
     }
   }
 }
+
 </script>
 <style lang="scss" scoped>
-.mask {
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.4);
-    position: fixed;
-    bottom: 0;
-    top:80px;
-    z-index: 1000;
-    .mask-content{
-        width:70%;
-        height:70%;
-        border-radius: 5px;
-        background: #fff;
-        position: relative;
-        top:50%;
-        left:50%;
-        transform:translate(-60%,-55%);
-        .padding-20{
-            padding:15px;
-            height:100%;
-            .box-header{
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                padding:0 10px;
-                height:40px;
-                line-height: 40px;
-                border-bottom:solid 1px #ccc;
-                .details{
-                    border-bottom:solid 2px #4977fc;
-                }
-                i{
-                    color:#ff6066;
-                    font-weight:bolder;
-                    display: inline-block;
-                }
-            }
-            .box-contenter{
-                display: flex;
-                padding:10px;
-                .box-content-left{
-                    width:186px;
-                    height:80%;
-                    background: #e8eeff;
-                }
-                .box-content-right{
-                    padding:10px;
-                    margin-left:10px;
-                    border:solid 1px #ccc;
-                }
-            }
+  .el-aside {
+    background-color: #e8eeff;
+    height: 400px;
+    .el-tree {
+      background-color:transparent;
+      & /deep/ .el-tree-node__content {
+        height:32px;
+
+      }
+      & /deep/ .el-tree-node:focus>.el-tree-node__content {
+        // background-color: transparent;
+      }
+      & /deep/ .el-tree-node__content:hover {
+        background-color: transparent;
+      }
+      & /deep/ .is-expanded {
+        & > .el-tree-node__content {
+          background-color:#d8e2ff;
+          border-top-right-radius: 16px;
+          border-bottom-right-radius: 16px;
+          margin-right:12px;
         }
+      }
+      & /deep/ .el-tree-node {
+        color:#595959;
+      }
+      & > .el-tree-node > .el-tree-node__content .el-tree-node{
+        color:#4977fc;
+      }
+      & /deep/ .el-tree-node.is-current {
+        color:#4977fc;
+        & > .el-tree-node__content > .is-leaf.el-icon-caret-right:before {
+          background-color:#4977fc;
+        }
+      }
+      & /deep/ .el-icon-caret-right:before {
+        content:'';
+        width:14px;
+        height:14px;
+        display:inline-block;
+        background:url('~@assets/imgs/right-arrow-blue-icon.png') center no-repeat;
+      }
+      & /deep/ .is-leaf.el-icon-caret-right:before {
+        content:'';
+        width:6px;
+        height:6px;
+        display:inline-block;
+        border-radius:50%;
+        background-color:#6e6e6e;
+        background-image: none;
+      }
     }
   }
+  .el-main {
+    border:1px solid #e8eeff;
+    margin-left:20px;
+  }
+  .dialog-container /deep/ .el-dialog__header {
+    padding-bottom: 0px;
+    position: relative;
+
+    .el-dialog__title {
+      border-bottom: 2px solid #4977fc;
+    }
+
+    &:after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 20px;
+      right: 20px;
+      height: 1px;
+      background-color: #ddd;
+    }
+
+    .el-dialog__headerbtn .el-dialog__close {
+      color: red;
+    }
+  }
+
+  .dialog-container /deep/ .el-dialog__body {
+    padding-top: 10px;
+
+  }
+
 </style>
